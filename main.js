8a083515b26406fe8b4ae01f8d5c18983f8926af
Johnny Castillo <johnnycast@gmail.com>
2018-05-03 23:52:52 -0600

    Embeded a couple of YouTube video players using the IFrame player API's Javascript functions.

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+
+  var instance = M.Carousel.init({
+      fullWidth: true,
+      indicators: true
+    });
+
+    // Or with jQuery
+
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true
+    });
+
+})
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: 'M7lc1UVf-VE',
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
