99e2bd7b168b2a8c162ec3248f9b44c620e3eba7
Johnny Castillo <johnnycast@gmail.com>
2018-05-05 10:21:07 -0600

    Added a new page and a carousel.

--- main.js
+++ main.js
@@ -27,3 +27,9 @@ function onYouTubeIframeAPIReady() {
     }
   });
 }
+
+$('.carousel-slider').carousel({fullWidth: true, padding:0},setTimeout(autoplay, 4500));
+ function autoplay() {
+   $('.carousel').carousel('next');
+   setTimeout(autoplay, 7500);
+    }
