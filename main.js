879193bb8c4d76da1d5e1a2ff86f04d2a97e4087
Johnny Castillo <johnnycast@gmail.com>
2018-05-08 16:04:58 -0600

    Added icons to the sidenav!, and an arrow to the carousel.

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
     height: '390',
     width: '640',
-    videoId: 'M7lc1UVf-VE',
+    videoId: 'player_uid_312879083_1',
     events: {
       'onReady': onPlayerReady,
       'onStateChange': onPlayerStateChange
