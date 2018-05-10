aa90e91c72ff0e479a3b7bb8dac6ca398494f6fe
Johnny Castillo <johnnycast@gmail.com>
2018-05-10 11:27:12 -0600

    BMI METER FIXES (PROBLEMS)

--- main.js
+++ main.js
@@ -72,25 +72,11 @@
         }
       })
     }
-   //making the bmi calculator//
-
-    function getYourBmi() {
-
-        var height = Number(document.getElementById("height").value);
-        var heightunits = document.getElementById("heightunits").value;
-
-        var BMI = weight / Math.pow(height, 2);
-
-    document.getElementById("output").innerHTML = Math.round(BMI * 100)/100;
-        if (BMI < 18.5) document.getElementById("comment").innerHTML = "Underweight";
-        if (BMI >= 18.5 && BMI <= 25) document.getElementById("comment").innerHTML = "Normal";
-        if (BMI >= 25 && BMI <= 30) document.getElementById("comment").innerHTML = "Obese";
-        if (BMI > 30) document.getElementById("comment").innerHTML = "Overweight";
-        document.getElementById("answer").value = output;
-     }
+  
 
 
 
-     M.AutoInit();
+
+
 
     });
